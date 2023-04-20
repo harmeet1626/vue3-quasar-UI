c1e4d284a1e718ce7e3e9cb609c1386f164ce3f5
Harmeet Singh <harmeet_singh1@softprodigy.com>
2023-04-20 18:48:43 +0530

    image added in userdetails page

--- src/pages/UserDetails.vue
+++ src/pages/UserDetails.vue
@@ -1,28 +1,33 @@
 <template>
-    <div class="q-pa-md" style="max-width: 400px">
-        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-            <div style="display: flex;">
-                <q-input style="width: 600px;" filled v-model="Firstname" label="First name" lazy-rules
+    <div style="display: flex;">
+        <div class="q-pa-md" style="max-width: 400px">
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+                <div style="display: flex;">
+                    <q-input style="width: 600px;" filled v-model="Firstname" label="First name" lazy-rules
+                        :rules="[val => val && val.length > 0 || 'Please type something']" />
+                    <q-input style="width: 600px;" filled type="number" v-model="age" label="Your age *" lazy-rules :rules="[
+                        val => val !== null && val !== '' || 'Please type your age',
+                        val => val > 0 && val < 100 || 'Please type a real age'
+                    ]" />
+                </div>
+                <div style="display: flex;">
+                    <q-input style="width: 600px;" filled v-model="gender" label="Gender" lazy-rules
+                        :rules="[val => val && val.length > 0 || 'Please type something']" />
+                    <q-input style="width: 600px;" filled v-model="Phone" label="Phone" lazy-rules
+                        :rules="[val => val && val.length > 0 || 'Please type something']" />
+                </div>
+                <q-input style="width: 350px;" filled v-model="birthdate" label="DOB" lazy-rules
                     :rules="[val => val && val.length > 0 || 'Please type something']" />
-                <q-input style="width: 600px;" filled type="number" v-model="age" label="Your age *" lazy-rules :rules="[
-                    val => val !== null && val !== '' || 'Please type your age',
-                    val => val > 0 && val < 100 || 'Please type a real age'
-                ]" />
-            </div>
-            <div style="display: flex;">
-                <q-input style="width: 600px;" filled v-model="gender" label="Gender" lazy-rules
-                    :rules="[val => val && val.length > 0 || 'Please type something']" />
-                <q-input style="width: 600px;" filled v-model="Phone" label="Phone" lazy-rules
-                    :rules="[val => val && val.length > 0 || 'Please type something']" />
-            </div>
-            <q-input style="width: 600px;" filled v-model="birthdate" label="DOB" lazy-rules
-                :rules="[val => val && val.length > 0 || 'Please type something']" />
-            <q-toggle style="width: 600px;" v-model="accept" label="I accept the license and terms" />
-            <div>
-                <q-btn label="Update" @click="update()" color="primary" />
-                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-            </div>
-        </q-form>
+                <q-toggle style="width: 600px;" v-model="accept" label="I accept the license and terms" />
+                <div>
+                    <q-btn label="Update" @click="update()" color="primary" />
+                    <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+                </div>
+            </q-form>
+        </div>
+        <img style="    height: 500px;
+        width: 700px;"
+            src="https://images.unsplash.com/photo-1603796846097-bee99e4a601f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZG9jdW1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60">
     </div>
 </template>
   
